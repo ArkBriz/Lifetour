0b3ed1f2cf451b0fe74aacbe84f14392dededd00
arkbriz <adbrizz@gmail.com>
2025-11-25 23:26:11 +0400

    fix bug with hero slider height on mobile

--- source/js/sliders/tours-slider.js
+++ source/js/sliders/tours-slider.js
@@ -1,7 +1,6 @@
 import Swiper from "swiper";
 import {Navigation} from "swiper/modules";
 import 'swiper/css';
-import 'swiper/css/pagination';
 
 new Swiper('.tours .swiper', {
   modules: [Navigation],
