6de2932207b2f95ccd8e5a377b9e7828ebd049df
arkbriz <adbrizz@gmail.com>
2025-11-25 21:28:33 +0400

    add hero section slider functionality

---
+++ source/js/promo-slider.js
@@ -0,0 +1,24 @@
+import Swiper from "swiper";
+import {Pagination} from "swiper/modules";
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+const promoSlider = new Swiper('.hero__slider', {
+  modules: [Pagination],
+  loop: true,
+  autoHeight: true,
+
+  pagination: {
+    el: '.hero__pagination',
+    type: 'bullets',
+  },
+
+  breakpoints: {
+    1440: {
+      pagination: {
+        clickable: true,
+      },
+      allowTouchMove: false,
+    },
+  },
+});
